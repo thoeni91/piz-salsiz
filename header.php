a3b3662028a6f5d7a47c0abea4765ed572dc2633
Marc Thöni <marcthoni@Marcs-Macbook-Pro.local>
2014-06-26 22:00:44 +0200

    bounce

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
     
 </head>
 <body <?php body_class(); ?>>
+<!-- test -->
     
 <header>
     <a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_url'); ?>/images/logo.png" alt="Piz Salsiz" /></a>
